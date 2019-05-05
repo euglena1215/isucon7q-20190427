e33e93b3affa8e07e62d68b0f8d275aeead43be7
Shintani Teppei <teppest1215@gmail.com>
2019-05-05 15:16:24 +0900

    🙏

--- isubata/webapp/ruby/redis_client.rb
+++ isubata/webapp/ruby/redis_client.rb
@@ -2,7 +2,7 @@ require 'redis'
 require 'redis/connection/hiredis'
 
 class RedisClient
+  @@redis = (Thread.current[:isu_redis] ||= Redis.new(host: (ENV["REDIS_HOST"] || "13.230.223.121"), port: 6379))
   class << self
-    @redis = (Thread.current[:isu_redis] ||= Redis.new(host: (ENV["REDIS_HOST"] || "13.230.223.121"), port: 6379))
   end
 end
