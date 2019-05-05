dc3150936758325b5bea841bd9eefd0ed411e63c
Shintani Teppei <teppest1215@gmail.com>
2019-05-05 15:30:14 +0900

    set localhost

--- isubata/webapp/ruby/redis_client.rb
+++ isubata/webapp/ruby/redis_client.rb
@@ -2,7 +2,7 @@ require 'redis'
 require 'redis/connection/hiredis'
 
 class RedisClient
-  @@redis = (Thread.current[:isu_redis] ||= Redis.new(host: (ENV["REDIS_HOST"] || "13.230.223.121"), port: 6379))
+  @@redis = (Thread.current[:isu_redis] ||= Redis.new(host: (ENV["REDIS_HOST"] || "127.0.0.1"), port: 6379))
   class << self
 
     def reset_last_message_id
