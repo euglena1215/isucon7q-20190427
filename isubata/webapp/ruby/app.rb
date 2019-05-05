f5c0f55f80206dc3279d69c31e8aadc6b44187ca
Shintani Teppei <teppest1215@gmail.com>
2019-05-05 11:31:59 +0900

    fix?

--- isubata/webapp/ruby/app.rb
+++ isubata/webapp/ruby/app.rb
@@ -176,13 +176,13 @@ class App < Sinatra::Base
       r = {}
       r['channel_id'] = channel_id
       r['unread'] = if row['haveread_user_id'].nil?
-        statement = db.prepare('SELECT COUNT(*) as cnt FROM message WHERE channel_id = ?')
-        statement.execute(channel_id).first['cnt']
+        statement2 = db.prepare('SELECT COUNT(*) as cnt FROM message WHERE channel_id = ?')
+        statement2.execute(channel_id).first['cnt']
       else
-        statement = db.prepare('SELECT COUNT(*) as cnt FROM message WHERE channel_id = ? AND ? < id')
-        statement.execute(channel_id, row['message_id']).first['cnt']
+        statement2 = db.prepare('SELECT COUNT(*) as cnt FROM message WHERE channel_id = ? AND ? < id')
+        statement2.execute(channel_id, row['message_id']).first['cnt']
       end
-      statement.close
+      statement2.close
       r
     end
 
